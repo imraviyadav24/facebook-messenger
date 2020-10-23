3c9e197ee083a3ad9cb9f2d311dc31baecddb2ec
ravi yadav <raviyadav00063@gmail.com>
2020-10-23 11:01:41 +0530

    the search field and the button is implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    // all the logic to send a message 
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Guys</h1>
+
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={sendMessage}>Send Message</button>
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+  ))
+      }
+
     </div>
   );
 }
